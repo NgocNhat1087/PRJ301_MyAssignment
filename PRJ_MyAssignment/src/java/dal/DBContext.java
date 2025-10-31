71cbafe5e667c3e85c36387998c22888379d1243
NgocNhat1087 <nhatngocvip@gmail.com>
2025-10-31 16:51:42 +0700

    Update DBContext.java

--- PRJ_MyAssignment/src/java/dal/DBContext.java
+++ PRJ_MyAssignment/src/java/dal/DBContext.java
@@ -22,7 +22,7 @@ public abstract class DBContext<T extends BaseModel> {
         try {
             String user = "sa";
             String pass = "123";
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ301_MyAssignment;encrypt=true;trustServerCertificate=true;";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=FALL25_Assignment_2;encrypt=true;trustServerCertificate=true;";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException ex) {
