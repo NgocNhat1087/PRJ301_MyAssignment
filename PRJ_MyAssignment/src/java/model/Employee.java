79e133646c2a2c3faaff4afa1a4d7c21f841ffe5
NgocNhat1087 <nhatngocvip@gmail.com>
2025-10-31 11:17:49 +0700

    Update Employee.java

--- PRJ_MyAssignment/src/java/model/Employee.java
+++ PRJ_MyAssignment/src/java/model/Employee.java
@@ -7,5 +7,62 @@ package model;
 
 
 public class Employee {
+    private String name;
+    private Department dept;
+    private Employee supervisor;
+    private String email;
+    private String phone;
 
+    public Employee(String name, Department dept, Employee supervisor, String email, String phone) {
+        this.name = name;
+        this.dept = dept;
+        this.supervisor = supervisor;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
+
+    public Employee getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Employee supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
 }
