3cfa80a1e73457769083d330d0fd3469e39e59f3
NgocNhat1087 <nhatngocvip@gmail.com>
2025-11-01 14:10:18 +0700

    Update ReviewController.java
    do get

--- PRJ_MyAssignment/src/java/controller/request/ReviewController.java
+++ PRJ_MyAssignment/src/java/controller/request/ReviewController.java
@@ -5,11 +5,13 @@
 package controller.request;
 
 import controller.iam.BaseRequiredAuthorizationController;
+import dal.RequestForLeaveDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.RequestForLeave;
 import model.iam.User;
 
 /**
@@ -25,6 +27,12 @@ public class ReviewController extends BaseRequiredAuthorizationController {
 
     @Override
     protected void processGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("id"));
+        RequestForLeaveDBContext db = new RequestForLeaveDBContext();
+        RequestForLeave rfl = db.get(id);
+        
+        req.setAttribute("rfl", rfl);
+        req.getRequestDispatcher("../view/request/review.jsp").forward(req, resp);
     }
     
 }
