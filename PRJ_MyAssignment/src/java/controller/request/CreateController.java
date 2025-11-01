e49e6ac2a47bbd3d68c7b6695bc5e23c0357e7cb
NgocNhat1087 <nhatngocvip@gmail.com>
2025-11-01 13:25:12 +0700

    Update CreateController.java

--- PRJ_MyAssignment/src/java/controller/request/CreateController.java
+++ PRJ_MyAssignment/src/java/controller/request/CreateController.java
@@ -5,13 +5,15 @@
 package controller.request;
 
 import controller.iam.BaseRequiredAuthorizationController;
+import dal.RequestForLeaveDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.RequestForLeave;
 import model.iam.User;
-
+import java.sql.Date;
 /**
  *
  * @author sonnt
@@ -21,7 +23,16 @@ public class CreateController extends BaseRequiredAuthorizationController {
 
     @Override
     protected void processPost(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-    
+        RequestForLeaveDBContext db = new RequestForLeaveDBContext();
+        RequestForLeave rfl = new RequestForLeave();
+        rfl.setFrom(Date.valueOf(req.getParameter("from")));
+        rfl.setTo(Date.valueOf(req.getParameter("to")));
+        rfl.setReason(req.getParameter("reason"));
+        rfl.setCreated_by(user.getEmployee());
+        
+        db.insert(rfl);
+        req.setAttribute("msg", "Create Success");
+        req.getRequestDispatcher("../view/request/create.jsp").forward(req, resp);
     }
 
     @Override
