6762e787cc42725624ed5c97c78802900cae2a9b
NgocNhat1087 <nhatngocvip@gmail.com>
2025-11-05 13:33:14 +0700

    Update ListController.java

--- PRJ_MyAssignment/src/java/controller/request/ListController.java
+++ PRJ_MyAssignment/src/java/controller/request/ListController.java
@@ -23,29 +23,30 @@ import model.iam.User;
 public class ListController extends BaseRequiredAuthorizationController {
 
     protected void processRequest(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
-        
+
         int eid = user.getEmployee().getId();
-        
+
         int pagesize = 10; // số đơn mỗi trang
         String page = req.getParameter("page");
-        page = (page==null)?"1":page;
+        page = (page == null) ? "1" : page;
         int pageindex = Integer.parseInt(page);
-        
-        
 
         RequestForLeaveDBContext db = new RequestForLeaveDBContext();
         ArrayList<RequestForLeave> rfls = db.list(eid, pageindex, pagesize);
         db = new RequestForLeaveDBContext();
         int totalRecords = db.count(eid);
-        int totalPages = (totalRecords % pagesize ==0)?totalRecords/pagesize:totalRecords/pagesize +1;
-        
+        int totalPages = (totalRecords % pagesize == 0) ? totalRecords / pagesize : totalRecords / pagesize + 1;
+
         req.setAttribute("rfls", rfls);
         req.setAttribute("pageindex", pageindex);
         req.setAttribute("totalPages", totalPages);
         req.setAttribute("action", "list");
         req.setAttribute("method", "GET");
+        req.setAttribute("pageTitle", "Request List");
+        req.setAttribute("contentPage", "../request/list.jsp");
+        req.getRequestDispatcher("/view/util/layout.jsp").forward(req, resp);
 
-        req.getRequestDispatcher("../view/request/list.jsp").forward(req, resp);
+//        req.getRequestDispatcher("../view/request/list.jsp").forward(req, resp);
     }
 
     @Override
